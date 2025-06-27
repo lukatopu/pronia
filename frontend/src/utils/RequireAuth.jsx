7972078cb395b9cdd611430ceb7ae3b32515753d
lukatopu <luksontopuria@gmail.com>
2025-06-27 18:38:49 +0400

    fixed requireAuth visual bug

--- frontend/src/utils/RequireAuth.jsx
+++ frontend/src/utils/RequireAuth.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { getCurrentUser } from '../api/api';
+import { useLoader } from '../hooks/useLoader';
 
 function RequireAuth({ children }) {
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
+  const { useFakeLoader } = useLoader();
 
   useEffect(() => {
     const checkAuth = async () => {
+      await useFakeLoader();
+
       try {
         await getCurrentUser();
         setAuthenticated(true);
